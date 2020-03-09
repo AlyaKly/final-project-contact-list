26d054b0f309de9a705dc06d67732f1ecbe5e4d9
Alina Klymenko <30704989+AlyaKly@users.noreply.github.com>
2020-03-09 15:03:44 +0100

    Added verification for empty fields on the Create form

--- js/main.js
+++ js/main.js
@@ -14,29 +14,58 @@ var app = new Vue({
       isShowModal: false,
       contactName: '',
       contactEmail: '',
-      contactPhone: ''
+      contactPhone: '',
+      nameError: false,
+      emailError: false,
+      phoneError: false,
+
     },
     // mounted: function() {
     //     this.createNewContact()
     // },
     methods: {
+        fieldsValidation: function () {
+            // no son vacios
+            this.nameError = false;
+            if (this.contactName === '') {
+                this.nameError = true;
+            }
+            this.emailError = false;
+            if (this.contactEmail === '') {
+                this.emailError = true;
+            }
+            this.phoneError = false;
+            if (this.contactPhone === '') {
+                this.phoneError = true;
+            }
+        },
         createNewContact: function() {
-            axios.post(URL_API_CONTACTS, {
-                fields: {
-                    Name: app.contactName,
-                    Email: app.contactEmail,
-                    Phone: app.contactPhone
-                }
-                
-            }, HEADERS)
-              .then(function (response) {
-                console.log(response);
-                app.isShowModal = false;
-                appTableContent.getTableContent();
-              })
-              .catch(function (error) {
-                console.log(error);
-              });
+            this.fieldsValidation();
+            if(!this.nameError && !this.emailError && !this.phoneError){
+                axios.post(URL_API_CONTACTS, {
+                    fields: {
+                        Name: app.contactName,
+                        Email: app.contactEmail,
+                        Phone: app.contactPhone
+                    }
+                    
+                }, HEADERS)
+                  .then(function (response) {
+                    console.log(response);
+                    app.isShowModal = false;
+                    appTableContent.getTableContent();
+                    app.emptyCreateForm();
+                  })
+                  .catch(function (error) {
+                    console.log(error);
+                  });
+            }
+
+        },
+        emptyCreateForm: function() {
+            app.contactName = '';
+            app.contactEmail = '';
+            app.contactPhone = '';
         }
     }
   });
@@ -57,7 +86,7 @@ var appTableContent = new Vue({
     computed: {
         contactSearch: function() {
             return this.contacts.filter(function(contact){
-                return contact.fields.Name.includes(appTableContent.searchName.charAt(0).toUpperCase() + appTableContent.searchName.slice(1))
+                return contact.fields.Name != undefined ? contact.fields.Name.includes(appTableContent.searchName.charAt(0).toUpperCase() + appTableContent.searchName.slice(1)) : []
             });
         }
     },
